b8ee6a87508e402f531b4b95fef5d9f3628eb6ce
David-again <d.ogunbanjo@yahoo.com>
2021-03-20 03:36:24 -0400

    boilerplate webpack configuration for development [no HTML autoreload yet]

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,11 @@
+import "../css/styles.css";
 // import { PopUp } from "./_modules-js/_annoying-alert.js";
 import { selfmultiply } from './_modules-js/_squared.js';
 console.log("Welcome!");
-console.log(selfmultiply(12));
+console.log(selfmultiply(11));
 
 // PopUp();
+
+if (module.hot) {
+    module.hot.accept()
+}
